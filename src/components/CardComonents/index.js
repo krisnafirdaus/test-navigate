a72113f9c17ac0a17f30fdfe4a38854006661da2
Krisna Firdaus <krisnafirdaus@gmail.com>
2023-08-30 22:14:53 +0800

    add feature mui

---
+++ src/components/CardComonents/index.js
@@ -0,0 +1,40 @@
+import React from "react"
+import {Card, CardMedia, CardHeader, CardContent, Typography, IconButton, Avatar} from '@mui/material';
+import {red} from '@mui/material/colors';
+import {MoreVert} from '@mui/icons-material';
+
+const CardComponents = () => {
+    return (
+        <Card sx={{ maxWidth: 345 }}>
+        <CardHeader
+          avatar={
+            <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+              R
+            </Avatar>
+          }
+          action={
+            <IconButton aria-label="settings">
+              <MoreVert />
+            </IconButton>
+          }
+          title="Shrimp and Chorizo Paella"
+          subheader="September 14, 2016"
+        />
+        <CardMedia
+          component="img"
+          height="194"
+          image="https://images.unsplash.com/photo-1687360440100-d4fbdc03bff9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHx8&auto=format&fit=crop&w=800&q=60"
+          alt="Paella dish"
+        />
+        <CardContent>
+          <Typography variant="body2" color="text.secondary">
+            This impressive paella is a perfect party dish and a fun meal to cook
+            together with your guests. Add 1 cup of frozen peas along with the mussels,
+            if you like.
+          </Typography>
+        </CardContent>
+      </Card>
+    )
+}
+
+export default CardComponents
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,28 @@
 import React from "react";
+import { Button, Checkbox, Alert } from '@mui/material';
+import CardComponents from '../../components/CardComonents';
+
+const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
 
 const Home = () => {
     return (
         <div>
             <h1>Home</h1>
+
+            <Button variant="contained" sx={{
+                backgroundColor: 'red',
+                ":hover": {
+                    backgroundColor: 'red'
+                }
+            }}>Click</Button>
+            <Checkbox {...label} />
+            <div>
+                <Alert severity="error">This is an error alert — check it out!</Alert>
+            </div>
+
+            <div>
+                <CardComponents />
+            </div>
         </div>
     )
 }
