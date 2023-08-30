60bd720dcb0b6cb8144e2a6c0cad283bab814c13
Krisna Firdaus <krisnafirdaus@gmail.com>
2023-08-30 22:34:22 +0800

    listing paa

--- src/components/CardComonents/index.js
+++ src/components/CardComonents/index.js
@@ -3,7 +3,7 @@ import {Card, CardMedia, CardHeader, CardContent, Typography, IconButton, Avatar
 import {red} from '@mui/material/colors';
 import {MoreVert} from '@mui/icons-material';
 
-const CardComponents = () => {
+const CardComponents = ({title, picture}) => {
     return (
         <Card sx={{ maxWidth: 345 }}>
         <CardHeader
@@ -23,14 +23,12 @@ const CardComponents = () => {
         <CardMedia
           component="img"
           height="194"
-          image="https://images.unsplash.com/photo-1687360440100-d4fbdc03bff9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHx8&auto=format&fit=crop&w=800&q=60"
+          image={picture}
           alt="Paella dish"
         />
         <CardContent>
           <Typography variant="body2" color="text.secondary">
-            This impressive paella is a perfect party dish and a fun meal to cook
-            together with your guests. Add 1 cup of frozen peas along with the mussels,
-            if you like.
+           {title}
           </Typography>
         </CardContent>
       </Card>
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,23 @@
 import React from "react";
-import { Button, Checkbox, Alert } from '@mui/material';
+import { Button, Checkbox, Alert, Grid } from '@mui/material';
 import CardComponents from '../../components/CardComonents';
 
 const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
 
 const Home = () => {
+    const data = [
+        {
+            id: 1,
+            gambar: "https://images.unsplash.com/photo-1693170509455-f28aefcd1005?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=800&q=60",
+            title: "Shrimp and Chorizo Paella",
+        },
+        {
+            id: 2,
+            gambar: "https://images.unsplash.com/photo-1693018011779-27f70a440a25?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=800&q=60",
+            title: "Shrimp and Chorizo Paellattttt",
+        },
+    ]
+
     return (
         <div>
             <h1>Home</h1>
@@ -21,7 +34,17 @@ const Home = () => {
             </div>
 
             <div>
-                <CardComponents />
+                <Grid container spacing={2}>
+                {data.map((item) => {
+                    console.log(item.title);
+                    return (
+                       <Grid item xs={3} key={item.id}>
+                         <CardComponents title={item.title}  picture={item.gambar}/>
+                       </Grid>
+                    )
+                })}
+                </Grid>
+              
             </div>
         </div>
     )
